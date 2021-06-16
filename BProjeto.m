23f3a8cd555f6cd9408f63fc3bc8303ea7556426
mrosario-o <86010101+mrosario-o@users.noreply.github.com>
2021-06-16 20:04:48 +0100

    Final version

---
+++ BProjeto.m
@@ -0,0 +1,122 @@
+clear;
+clc;
+
+%Análise do método de Jacobi B1) e B2)
+
+%Properties:
+L = 1; %Width
+tol = 1e-7; %Tolerance
+
+h = [0.02 0.025 0.03125 0.04 0.05 0.0625 0.1 0.125 0.2 0.25 0.5]; %Diferent steps
+%..................................................................................
+
+
+[M,n_Iter,time] = deal(zeros(1,length(h)));
+
+for hIndex = 1:length(h)
+
+    N = (2*L/h(hIndex))+1; % Or N = length(-L:h(hIndex):L);
+    M(hIndex) = N;
+    V_old = zeros(N,N);
+
+
+    %{
+    Mathematicaly:
+    Coords(index) = (2*L*(index-1))/(N-1)-L
+    IndexN(coordinate) = (coordinate+L)*(N-1)/(2*L)+1
+
+    Como N = (2*L/h(hIndex))+1
+    Coords(index) = h(hIndex)*(index-1) - L
+    IndexN(coordinate) = (coordinate+L)/h(hIndex) + 1
+    %}
+
+
+    for xIndex=1:N
+        for yIndex = 1:N
+            if xIndex == N || xIndex == 1
+                V_old(xIndex,yIndex) = (h(hIndex)*(yIndex-1)-L)/L; %Or V_old(xIndex,yIndex) = h(hIndex)*(yIndex-((N+1)/2))/L;
+            end
+
+            if yIndex == N
+                V_old(xIndex,yIndex) = 1;
+            end
+            if yIndex == 1
+                V_old(xIndex,yIndex) = -1;
+            end
+        end
+    end
+
+    %{
+    De um modo alternativo:
+    (Tem a vantagem de usar coordenadas em vez de indices como o anterior)
+    for x = -L:h(hIndex):L
+        for y = -L:h(hIndex):L
+            if x == L || x == -L
+                V_old((x+L)/h(hIndex) + 1,(y+L)/h(hIndex) + 1) = y/L;
+            end
+
+            if y == L
+                V_old((x+L)/h(hIndex) + 1,(y+L)/h(hIndex) + 1) = 1;
+            end
+            if y == -L
+                V_old((x+L)/h(hIndex) + 1,(y+L)/h(hIndex) + 1) = -1;
+            end
+        end
+    end
+    %}
+
+    V_new = V_old;
+
+    nIte = 0;
+    tic;
+    while true
+        nIte = nIte + 1; %Incremento de número de iterações
+
+        for xIndex = 2:N-1 %Fronteiras não alteradas
+            for yIndex = 2:N-1
+                V_new(xIndex,yIndex)=(V_old(xIndex,yIndex+1)+V_old(xIndex,yIndex-1)+V_old(xIndex+1,yIndex)+V_old(xIndex-1,yIndex))/4;
+            end
+        end
+
+        if (sqrt(sum(sum((V_new-V_old).^2)))/sqrt(sum(sum(V_new.^2)))) < tol %Condição de tolerância
+            n_Iter(hIndex) = nIte;
+            break
+        end
+
+        V_old = V_new;
+    end
+    
+    time(hIndex) = toc;
+end
+
+x = log(M);
+yi = log(n_Iter);
+yt = log(time);
+
+pn = polyfit(x,yi,1);
+pt = polyfit(x,yt,1);
+
+figure;
+subplot(2,2,1);
+plot(x,yi,'-o');
+title('Dados do número de iterações');
+xlabel('Logarítmo de M');
+ylabel('Logarítmo de nIte');
+
+subplot(2,2,2);
+plot(x,polyval(pn,x));
+title(strcat('Polyfit do número de iterações (m=',num2str(pn(1)),')'));
+xlabel('Logarítmo de M');
+ylabel('Logarítmo de nIte');
+
+subplot(2,2,3);
+plot(x,yt,'-o');
+title('Dados do tempo de iteração');
+xlabel('Logarítmo de M');
+ylabel('Logarítmo do tempo');
+
+subplot(2,2,4);
+plot(x,polyval(pt,x));
+title(strcat('Polyfit do tempo de iteração (m=',num2str(pt(1)),')'));
+xlabel('Logarítmo de M');
+ylabel('Logarítmo do tempo');
---
+++ EProjeto.m
@@ -0,0 +1,111 @@
+clear;
+clc;
+
+%Análise do campo elétrico C)
+
+%Properties:
+L = 1; %Width
+h = 0.05; %Step
+tol = 1e-7; %Tolerance
+
+R = 1/2; %Cylinder's radius
+P = [1/4 1/8]; %Cylinder's center [x y]
+%........................................
+
+
+N = (2*L/h)+1; % Or N = length(-L:h:L);
+alphaOpt = (2/(1+(pi/N)));
+V_old = zeros(N,N);
+
+
+%{
+Mathematicaly:
+Coords(index) = (2*L*(index-1))/(N-1)-L
+IndexN(coordinate) = (coordinate+L)*(N-1)/(2*L)+1
+
+Como N = (2*L/h)+1
+Coords(index) = h*(index-1) - L
+IndexN(coordinate) = (coordinate+L)/h + 1
+%}
+
+
+for xIndex=1:N
+    for yIndex = 1:N
+        if xIndex == N || xIndex == 1
+            V_old(xIndex,yIndex) = (h*(yIndex-1)-L)/L; %Or V_old(xIndex,yIndex) = h*(yIndex-((N+1)/2))/L;
+        end
+        
+        if yIndex == N
+            V_old(xIndex,yIndex) = 1;
+        end
+        if yIndex == 1
+            V_old(xIndex,yIndex) = -1;
+        end
+    end
+end
+
+%{
+De um modo alternativo:
+(Tem a vantagem de usar coordenadas em vez de indices como o anterior)
+for x = -L:h:L
+    for y = -L:h:L
+        if x == L || x == -L
+            V_old((x+L)/h + 1,(y+L)/h + 1) = y/L;
+        end
+        
+        if y == L
+            V_old((x+L)/h + 1,(y+L)/h + 1) = 1;
+        end
+        if y == -L
+            V_old((x+L)/h + 1,(y+L)/h + 1) = -1;
+        end
+    end
+end
+%}
+
+V_new = V_old;
+
+nIte = 0;
+while true
+    nIte = nIte + 1; %Incremento de número de iterações
+    
+    for xIndex = 2:N-1 %Fronteiras não alteradas
+        for yIndex = 2:N-1
+            if (h*(xIndex-1) - L - P(1))^2+(h*(yIndex-1) - L - P(2))^2 <= R^2
+                %Condição do cilíndro (x^2+y^2<=R^2)
+                V_new(xIndex,yIndex)=(1-alphaOpt)*V_old(xIndex,yIndex)+alphaOpt*(V_new(xIndex,yIndex+1)+V_new(xIndex,yIndex-1)+V_new(xIndex+1,yIndex)+V_new(xIndex-1,yIndex)+100*h^2)/4;
+            else
+                V_new(xIndex,yIndex)=(1-alphaOpt)*V_old(xIndex,yIndex)+alphaOpt*(V_new(xIndex,yIndex+1)+V_new(xIndex,yIndex-1)+V_new(xIndex+1,yIndex)+V_new(xIndex-1,yIndex))/4;
+            end
+        end
+    end
+    
+    if (sqrt(sum(sum((V_new-V_old).^2)))/sqrt(sum(sum(V_new.^2)))) < tol %Condição de tolerância
+        [X,Y] = meshgrid(-L:h:L);
+        
+        figure;
+        mesh(X,Y,V_new);
+        
+        title('Potencial na superfície');
+        xlabel('Eixo y');
+        ylabel('Eixo x');
+        zlabel('Diferença de potencial');
+        break
+    end
+    
+    V_old = V_new;
+end
+
+fprintf('Número de iterações: %d.\n',nIte);
+
+[Ex,Ey] = gradient(V_new,h,h);
+Ex = -Ex; Ey = -Ey; %Dado que os vectores têm o sentido oposto
+
+figure;
+quiver(X,Y,Ex,Ey,'Color','r');
+grid on;
+axis equal;
+
+title('Campo elétrico');
+xlabel('Eixo y');
+ylabel('Eixo x');
